4d6753dabaa428b7eaab23ae20cc8b1e77c69613
sprin <sprin>
2013-11-23 13:36:35 -0800

    Create query executor that constructs request from endpoint config
    Capable of JSON and JSONP, as specified in endpoint config.

---
+++ src/query_executor.js
@@ -0,0 +1,40 @@
+// Query Executor
+define(function (require) {
+  var _ = require('underscore'),
+    $ = require('jquery');
+
+  var configure_query_executor = function(options){
+    var api_url = options.api_url;
+    var data_type = options.data_type;
+    var param_map = options.param_map;
+    var default_params = options.default_params;
+
+    var execute_query = function(options) {
+      var search = options.search;
+      var domain = options.domain;
+      var success = options.success;
+
+      // Create the initial data object by extending the default params
+      var data = _.extend({}, default_params);
+
+      // Add the search param, and optionally the domain param to data object.
+      data[param_map['search']] = search;
+      if (domain) {
+        data[param_map['domain']] = domain;
+      }
+
+      $.ajax({
+        dataType: data_type,
+        url: api_url,
+        data: data,
+        success: success,
+      });
+    };
+
+    return execute_query;
+  };
+
+  return configure_query_executor;
+
+});
+
--- src/searchthedocs.js
+++ src/searchthedocs.js
@@ -4,7 +4,8 @@ define(function (require) {
     $ = require('jquery'),
     Backbone = require('backbone'),
     Navbar = require('./navbar'),
-    Sidebar = require('./sidebar');
+    Sidebar = require('./sidebar'),
+    configure_query_executor = require('./query_executor');
 
   var SearchTheDocsView = Backbone.View.extend({
 
@@ -16,6 +17,25 @@ define(function (require) {
       var t = this;
       t.brand = options.brand;
       t.brand_href = options.brand_href;
+      t.search_options = options.search_options;
+
+      // Default endpoint
+      t.ep_name = t.search_options.default_endpoint;
+      t.set_endpoint(t.ep_name);
+
+    },
+
+    set_endpoint: function(ep_name) {
+      var t = this;
+      t.ep = t.search_options.endpoints[ep_name];
+
+      // Configure query executor
+      t.execute_query = configure_query_executor({
+        api_url: t.ep.api_url,
+        data_type: t.ep.data_type,
+        param_map: t.ep.param_map,
+        default_params: t.ep.default_params
+      });
     },
 
     render: function() {
